929df8af7c6fac5d0324a13960edf56d77dc589c
Benoit Daloze <eregontp@gmail.com>
2016-04-22 13:50:29 +0200

    check

--- test.rb
+++ test.rb
@@ -4,15 +4,11 @@ def show(code)
 end
 
 # New file
-File.open("abc", "w", 0666) { |f|
+File.open("abc", "w", 0444) { |f|
   puts f.stat.mode.to_s(8)
 }
 
 # File exists
-File.open("abc", "w", 0466) { |f|
+File.open("abc", "w", 0666) { |f|
   puts f.stat.mode.to_s(8)
 }
-
-File.open("def", "w", 0444) { |f| f.write "123" }
-p File.read("def")
-puts File.stat("def").mode.to_s(8)

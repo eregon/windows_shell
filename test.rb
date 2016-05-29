ca9934f3315162b6d1f434371f3b817c94f076d0
Benoit Daloze <eregontp@gmail.com>
2016-05-29 12:47:04 +0200

    investigate

--- test.rb
+++ test.rb
@@ -7,10 +7,17 @@ File.open("abc", "wb") { |f|
   f.write "a\r\n"
 }
 
-File.open("abc", "rb") { |f|
-  p f.read
+File.open("abc", "rb") { |f| p f.read }
+p File.read("abc")
+p File.binread("abc")
+
+puts
+
+File.open("abc", "wb", newline: :crlf) { |f|
+  f.write "a\n"
+  f.puts "b"
 }
 
+File.open("abc", "rb") { |f| p f.read }
 p File.read("abc")
-
 p File.binread("abc")

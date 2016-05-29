ce17698b4370508a628956583241e77894b5ddc7
Benoit Daloze <eregontp@gmail.com>
2016-05-29 13:23:10 +0200

    try2

--- test.rb
+++ test.rb
@@ -3,8 +3,9 @@ def show(code)
   p eval(code)
 end
 
-File.open("abc", "wb") { |f|
-  f.write "a\r\n"
+File.open("abc", "wt", newline: :lf) { |f|
+  f.write "a\n"
+  f.puts "b"
 }
 
 File.open("abc", "rb") { |f| p f.read }
@@ -13,7 +14,7 @@ p File.binread("abc")
 
 puts
 
-File.open("abc", "wt", newline: :crlf) { |f|
+File.open("abc", "w", newline: :lf) { |f|
   f.write "a\n"
   f.puts "b"
 }

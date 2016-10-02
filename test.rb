18d109a74c5a68ba9f1ed6a7fb8037cfb83a3e1d
Benoit Daloze <eregontp@gmail.com>
2016-10-02 14:57:37 +0200

    zero? for a dir

--- test.rb
+++ test.rb
@@ -3,22 +3,12 @@ def show(code)
   p eval(code)
 end
 
-File.open("abc", "wt", newline: :lf) { |f|
-  f.write "a\n"
-  f.puts "b"
-}
+Dir.mkdir("foo")
+D = "foo"
+p D
+show "File.zero?(D)"
+show "File.stat(D).zero?"
+show "File.lstat(D).zero?"
+show "FileTest.zero?(D)"
 
-File.open("abc", "rb") { |f| p f.read }
-p File.read("abc")
-p File.binread("abc")
-
-puts
-
-File.open("abc", "w", newline: :lf) { |f|
-  f.write "a\n"
-  f.puts "b"
-}
-
-File.open("abc", "rb") { |f| p f.read }
-p File.read("abc")
-p File.binread("abc")
+Dir.rmdir D

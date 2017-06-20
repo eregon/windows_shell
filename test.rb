f15f171c7fd2783c64b7fb05374fddb4a2f19807
Benoit Daloze <eregontp@gmail.com>
2017-06-20 13:58:37 +0200

    echo env

--- test.rb
+++ test.rb
@@ -5,3 +5,9 @@ end
 
 show "RbConfig::CONFIG['host_os']"
 show "RUBY_PLATFORM"
+
+ENV["FOO"] = "BAR"
+
+show 'Process.wait Process.spawn("echo $FOO")'
+show 'Process.wait Process.spawn("echo %FOO%")'
+show 'Process.wait Process.spawn("echo", "%FOO%")'

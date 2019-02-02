fc7ca8c16d43d6396bead459ebbefd306cca3988
Benoit Daloze <eregontp@gmail.com>
2019-02-02 12:53:05 +0100

    dlext

--- test.rb
+++ test.rb
@@ -8,14 +8,4 @@ end
 show "RbConfig::CONFIG['host_os']"
 show "RUBY_PLATFORM"
 
-puts
-
-Process.constants.select { |c| c.to_s.start_with?('CLOCK_') }.each do |c|
-  v = Process.const_get(c)
-  p c => v
-  begin
-    p Process.clock_gettime(v)
-  rescue Errno::EINVAL => e
-    p e.message
-  end
-end
+show "RbConfig::CONFIG['DLEXT']"
